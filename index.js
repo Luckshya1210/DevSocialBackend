c1893c8013dd53b77777ce41b2193fe167ffd0ef
Luckshya <luckshya1210@gmail.com>
2023-06-24 00:32:58 +0530

    c

--- index.js
+++ index.js
@@ -5,19 +5,21 @@ import mongoose from 'mongoose'
 import postRoutes from './routes/posts.js'
 import userRoutes from './routes/users.js'
 import dotenv from 'dotenv'
-const app=express()
+const app=express();
 
-dotenv.config()
-
-app.use(bodyParser.json({limit:"30mb",extended:true}))
-app.use(bodyParser.urlencoded({limit:"30mb",extended:true}))
+dotenv.config();
+app.use(bodyParser.json({limit:"30mb",extended:true}));
+app.use(bodyParser.urlencoded({limit:"30mb",extended:true}));
 app.use(cors());
-
-app.use('/posts',postRoutes)
-app.use('/user',userRoutes)
+app.use(express.json());
+app.use('/posts',postRoutes);
+app.use('/user',userRoutes);
+app.get("/",(req,res)=>{
+    res.send("<h1>Working</h1>");
+});
 const CONNECTION=process.env.URL
 const PORT=process.env.PORT || 8800
-mongoose.set('strictQuery', true)
+mongoose.set('strictQuery', true);
 mongoose.connect(CONNECTION,{useNewUrlParser:true,useUnifiedTopology:true})
     .then(()=>app.listen(PORT,()=>console.log(`Server running on port ${PORT}`)))
     .catch((error)=>console.log(error.message))
